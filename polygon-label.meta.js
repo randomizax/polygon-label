244790ba08a72ae0dc20c13f146dd422af846450
Random Xyzzy <rndmxyzzy@gmail.com>
2014-12-08 01:40:35 +0900

    bump

---
+++ polygon-label.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @id             iitc-plugin-polygon-label@randomizax
+// @name           IITC plugin: Polygon Labels
+// @category       Layer
+// @version        0.1.1.20141208.013956
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://rawgit.com/randomizax/polygon-label/latest/polygon-label.meta.js
+// @downloadURL    https://rawgit.com/randomizax/polygon-label/latest/polygon-label.user.js
+// @description    [randomizax-2014-12-08-013956] Show portal hack details on map.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
--- polygon-label.user.js
+++ polygon-label.user.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-polygon-label@randomizax
 // @name           IITC plugin: Polygon Labels
 // @category       Layer
-// @version        0.1.0.20141207.233816
+// @version        0.1.1.20141208.013956
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://rawgit.com/randomizax/polygon-label/latest/polygon-label.meta.js
 // @downloadURL    https://rawgit.com/randomizax/polygon-label/latest/polygon-label.user.js
-// @description    [randomizax-2014-12-07-233816] Show portal hack details on map.
+// @description    [randomizax-2014-12-08-013956] Show portal hack details on map.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
